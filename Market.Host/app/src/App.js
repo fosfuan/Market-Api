a20d19e3aaad7e0bea5c70f3b3db87995538b0ef
fosfuan <wojci.kraw@gmail.com>
2018-03-12 20:22:45 +0100

    basic route again, and material ui

--- Market.Host/app/src/App.js
+++ Market.Host/app/src/App.js
@@ -1,40 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import * as testActions from './actions/testActions.js';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import { grey900, cyan500, cyan700, grey400 } from 'material-ui/styles/colors';
 
 class App extends React.Component {
-constructor(props, context) {
-        super(props, context);
-    }
+  constructor(props, context) {
+    super(props, context);
+  }
 
   render() {
+  const customTheme = {
+    palette: { 
+      primary1Color: cyan500,
+      primary2Color: cyan700,
+      primary3Color: grey400
+    }
+  };
+
+  const theme = getMuiTheme(customTheme);
+
     return (
-      <MuiThemeProvider>
-          <div className="App">
-            {this.props.test}
-            {this.props.categories.map((category, index) => {
-                return (
-                  <div key={index}>
-                    <p>{category}</p>
-                  </div>
-                );
-              }            
-            )}
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div>
+          <AppBar title="My AppBar" />
+          <div className="container-fluid" >
+            <div>{this.props.children}</div>
           </div>
+        </div>
       </MuiThemeProvider>
     );
   }
 }
 
 App.propTypes = {
-    test: PropTypes.string,
-    categories: PropTypes.array
+  test: PropTypes.string,
+  categories: PropTypes.array,
+  children: PropTypes.object.isRequired
 }
 
 function mapStateToProps(state) {
@@ -44,10 +53,10 @@ function mapStateToProps(state) {
   }
 }
 
-function mapispatchToProps(dispatch){
-    return{
-        actions: bindActionCreators(testActions, dispatch)
-    };
+function mapispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(testActions, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapispatchToProps)(App);
--- Market.Host/app/src/index.js
+++ Market.Host/app/src/index.js
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './stores/configureStore';
 import {Provider} from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
-import routes from './route';
-import './index.css';
+import routes from './routes';
 import App from './App';
 import About from './components/about/AboutPage';
 import registerServiceWorker from './registerServiceWorker';
@@ -16,12 +15,16 @@ const store = configureStore();
 store.dispatch(loadTests());
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App} />
-      <Route path="/about" component={About} />
-    </Router>
-  </Provider>,
+<Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+</Provider>,
+  // <Provider store={store}>
+  //   <Router history={browserHistory}>
+  //     <Route path="/" component="App">
+  //         <Route path="/about" component={About} />
+  //     </Route>
+  //   </Router>
+  // </Provider>,
   document.getElementById('root'));
 
 registerServiceWorker();
