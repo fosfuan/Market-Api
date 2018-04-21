a7ad4832cf25553e76de53e3e3a8fa231847adce
unknown <wojci.kraw@gmail.com>
2018-04-21 12:15:42 +0200

    Custom Model Validation (for test purpose)

--- Market.API/CryptoMaket/CryptoMaket/Models/SkipTakeModel.cs
+++ Market.API/CryptoMaket/CryptoMaket/Models/SkipTakeModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EFMarket.DAL;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,41 @@ namespace CryptoMaket.Models
 {
     public class SkipTakeModel
     {
+        [SkipCheck]
         public int Skip { get; set; }
         public int Take { get; set; }
+    }
+
+    public class SkipCheck : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var service = (IUnitOfWork)validationContext
+                                 .GetService(typeof(IUnitOfWork));
+
+            var model = validationContext.ObjectInstance as SkipTakeModel;
+            var test = service.UserRepository.GetUserById(10);
+
+            if (model == null)
+                throw new ArgumentException("Attribute not applied on Employee");
+
+            if (model.Skip < 10)
+                return new ValidationResult(GetErrorMessage(validationContext));
+
+            return ValidationResult.Success;
+        }
+
+        private string GetErrorMessage(ValidationContext validationContext)
+        {
+            // Message that was supplied
+            if (!string.IsNullOrEmpty(this.ErrorMessage))
+                return this.ErrorMessage;
+
+            // Use generic message: i.e. The field {0} is invalid
+            //return this.FormatErrorMessage(validationContext.DisplayName);
+
+            // Custom message
+            return $"{validationContext.DisplayName} can't be smaller than 10";
+        }
     }
 }
